9a5ba96ef324d02b486c2b3cc50ed620be20e074
Don-Uherka <uherkadon@yahoo.com>
2021-06-01 15:45:05 -0500

    DU: I added the hashCode method and the equals method to the order class

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -28,5 +28,37 @@ public class Order {
 	public String toString() {
 		return "id: " + orderId + "Customer id: " + customer.getId();
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		result = prime * result + ((customer.getId() == null) ? 0 : customer.getId().hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (getId() == null) {
+			if (other.getId() != null)
+				return false;
+		} else if (!getId().equals(other.getId()))
+			return false;
+		if (getCustomerId() == null) {
+			if (other.getCustomerId() != null)
+				return false;
+		} else if (!getCustomerId().equals(other.getCustomerId()))
+			return false;
+		return true;
+		
+	}
 
 }
